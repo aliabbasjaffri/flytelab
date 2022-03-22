09e77cb70ebb4d87760adae1ab928eba96d5c6e7
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-03-22 23:48:31 +0100

    Added data preprocessing step

---
+++ projects/music_genre_classification/music_genre_classification/preprocess.py
@@ -0,0 +1,66 @@
+import os
+import errno
+import pathlib
+from pydub import AudioSegment
+from torchaudio.datasets.gtzan import gtzan_genres
+
+
+data_dir = "data/"
+genre_dir = "genres/"
+max_sec = 28 * 1000  # max size of audio we take (for 28 sec)
+segment = 2 * 1000  # segment size(for 2 sec)
+counter = 0
+
+
+def generate_directory(directory_path: str) -> None:
+    try:
+        pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+
+def export_to_dir(class_name, data_dir, chopped_audio, counter):
+    for audio_data in chopped_audio:
+        file_name = class_name + "_" + str(counter) + ".wav"
+        export_path = os.path.join(data_dir, file_name)
+        audio_data.export(export_path, format="wav")
+        int(counter)
+        counter += 1
+    return counter
+
+
+def chop_audio(
+    audio_file: str, segment_size: int = segment, max_size: int = max_sec
+):  # All file are not even so we cut it in same size using max_size and provide segment size using segment
+    audio_data = AudioSegment.from_wav(file=audio_file)
+    cut_audio_data = audio_data[:max_size]
+    chopped_audio = [x for x in cut_audio_data[::segment_size]]
+    return chopped_audio
+
+
+def count_items_in_dataset() -> None:
+    number_files = len(os.listdir(data_dir))
+    print(number_files)
+
+
+def preprocess_data() -> None:
+    generate_directory(data_dir)
+
+    _counter = 0
+
+    if len(os.listdir(data_dir)) == 0:
+        for genre in gtzan_genres:
+            path = os.path.join(genre_dir, genre)
+            audio_files = os.listdir(path)
+            for audio_file in audio_files:
+                chopped_audio = chop_audio(audio_file=os.path.join(path, audio_file))
+                _counter = export_to_dir(genre, data_dir, chopped_audio, _counter)
+                print(audio_file)
+    else:
+        print("data already preprocessed")
+
+
+if __name__ == "__main__":
+    preprocess_data()
+    count_items_in_dataset()
