219827d0fc914c7fa42d6e2b31bc883711dcce33
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-03-25 23:37:36 +0100

    Updated datasource

--- projects/music_genre_classification/music_genre_classification/datasource.py
+++ projects/music_genre_classification/music_genre_classification/datasource.py
@@ -1,88 +1,20 @@
 import torch
 from torchaudio.datasets import GTZAN
-
-# from torchvision.datasets import ImageFolder
 import torchaudio.transforms as tt
-from torch.utils.data import random_split
-
-# from torchaudio.utils import make_grid
 import matplotlib
 import matplotlib.pyplot as plt
-from torch.utils.data import DataLoader
-from dataset import MusicDataset
-from preprocess import data_dir
-from utils import get_default_device, DeviceDataLoader
+from torch.utils.data import Dataset
 
 random_seed = 42
 torch.manual_seed(random_seed)
-
 matplotlib.rcParams["figure.facecolor"] = "#ffffff"
 
 
-def download_raw_dataset():
-    _dataset = GTZAN(root=".", download=True)
+def download_raw_dataset(
+    target_folder: str = ".", download_dataset: bool = True
+) -> Dataset:
+    _dataset = GTZAN(root=target_folder, download=download_dataset)
     return _dataset
-
-
-def generate_data_split(
-    batch_size: int = 150,
-    train_split_pc: float = 0.6,
-    validation_split_pc: float = 0.2,
-    test_split_pc: float = 0.2,
-) -> (DataLoader, DataLoader, DataLoader):
-    _dataset = MusicDataset(data_dir)
-
-    assert (train_split_pc + validation_split_pc + test_split_pc) == 1
-
-    train_split_size = int(len(_dataset) * train_split_pc)
-    validation_split_size = int(len(_dataset) * validation_split_pc)
-    test_split_size = int(len(_dataset) * test_split_pc)
-
-    train_dataset, validation_dataset, test_dataset = random_split(
-        _dataset, [train_split_size, validation_split_size, test_split_size]
-    )
-
-    print(
-        f"Training Dataset size: {len(train_dataset)}, "
-        f"Validation Dataset size: {len(validation_dataset)}, "
-        f"Test Dataset size: {len(test_dataset)}"
-    )
-
-    device = get_default_device()
-
-    train_dataloader = DeviceDataLoader(
-        dataloader=DataLoader(
-            dataset=train_dataset,
-            batch_size=batch_size,
-            shuffle=True,
-            num_workers=4,
-            pin_memory=True,
-        ),
-        device=device,
-    )
-
-    validation_dataloader = DeviceDataLoader(
-        dataloader=DataLoader(
-            dataset=validation_dataset,
-            batch_size=batch_size * 2,
-            num_workers=4,
-            pin_memory=True,
-        ),
-        device=device,
-    )
-
-    test_dataloader = DeviceDataLoader(
-        dataloader=DataLoader(
-            dataset=test_dataset,
-            batch_size=1,
-            shuffle=True,
-            num_workers=4,
-            pin_memory=True,
-        ),
-        device=device,
-    )
-
-    return train_dataloader, validation_dataloader, test_dataloader
 
 
 if __name__ == "__main__":
@@ -104,5 +36,3 @@ if __name__ == "__main__":
     plt.imshow(spectrogram.log2()[0, :, :].numpy(), cmap="magma")
 
     # plt.show()
-
-    train_dataloader, validation_dataloader, test_dataloader = generate_data_split()
