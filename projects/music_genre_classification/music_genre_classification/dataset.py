ce0e93dd17180db79f0bffefb481c72efb0a0ebb
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-03-25 23:40:51 +0100

    added dataset logic

--- projects/music_genre_classification/music_genre_classification/dataset.py
+++ projects/music_genre_classification/music_genre_classification/dataset.py
@@ -1,23 +1,17 @@
 import os
 import torchaudio
-from preprocess import data_dir
+from utils import parse_genres
 from torch.utils.data import Dataset
 
 
-def parse_genres(filename):
-    parts = filename.split("_")
-    return " ".join(parts[:-1])
-
-
 class MusicDataset(Dataset):
-    def __init__(self, root):
+    def __init__(self, root: str = "data/"):
         super().__init__()
         self.root = root
         self.files = [
             filename for filename in os.listdir(root) if filename.endswith(".wav")
         ]
         self.classes = list(set(parse_genres(filename) for filename in self.files))
-        # self.transform = transform
 
     def __len__(self):
         return len(self.files)
@@ -25,13 +19,12 @@ class MusicDataset(Dataset):
     def __getitem__(self, i):
         filename = self.files[i]
         fpath = os.path.join(self.root, filename)
-        # img = self.transform(open_image(fpath))
         audio = torchaudio.load(fpath)[0]
         class_idx = self.classes.index(parse_genres(filename))
         return audio, class_idx
 
 
 if __name__ == "__main__":
-    dataset = MusicDataset(data_dir)
+    dataset = MusicDataset()
     print(f"Length of dataset: {len(dataset)}")
     print(dataset[0])
