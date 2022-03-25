a015eb6c1dbf76577b60192fa30ec6cab5d1c0b2
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-03-25 23:36:48 +0100

    Added util fucntions

--- projects/music_genre_classification/music_genre_classification/utils.py
+++ projects/music_genre_classification/music_genre_classification/utils.py
@@ -1,11 +1,15 @@
 import torch
+import errno
+import pathlib
 
 
 def get_default_device():
     """Pick GPU if available, else CPU"""
     if torch.cuda.is_available():
+        print("running to cuda device...")
         return torch.device("cuda")
     else:
+        print("running on CPU")
         return torch.device("cpu")
 
 
@@ -31,3 +35,16 @@ class DeviceDataLoader:
     def __len__(self):
         """Number of batches"""
         return len(self.dataloader)
+
+
+def generate_directory(directory_path: str) -> None:
+    try:
+        pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
+    except OSError as exception:
+        if exception.errno != errno.EEXIST:
+            raise
+
+
+def parse_genres(filename):
+    parts = filename.split("_")
+    return " ".join(parts[:-1])
