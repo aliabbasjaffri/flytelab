410839bab2aa689c08b0c960376db2273c7d9dc8
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-03-25 23:38:40 +0100

    Updated preprocess

--- projects/music_genre_classification/music_genre_classification/preprocess.py
+++ projects/music_genre_classification/music_genre_classification/preprocess.py
@@ -1,23 +1,12 @@
 import os
-import errno
-import pathlib
 from pydub import AudioSegment
+from utils import generate_directory
 from torchaudio.datasets.gtzan import gtzan_genres
 
-
-data_dir = "data/"
+preprocessed_data_dir = "data/"
 genre_dir = "genres/"
 max_sec = 28 * 1000  # max size of audio we take (for 28 sec)
 segment = 2 * 1000  # segment size(for 2 sec)
-counter = 0
-
-
-def generate_directory(directory_path: str) -> None:
-    try:
-        pathlib.Path(directory_path).mkdir(parents=True, exist_ok=True)
-    except OSError as exception:
-        if exception.errno != errno.EEXIST:
-            raise
 
 
 def export_to_dir(class_name, data_dir, chopped_audio, counter):
@@ -39,23 +28,18 @@ def chop_audio(
     return chopped_audio
 
 
-def count_items_in_dataset() -> None:
-    number_files = len(os.listdir(data_dir))
-    print(number_files)
-
-
-def preprocess_data() -> None:
-    generate_directory(data_dir)
-
+def preprocess_data(preprocessed_data_dir: str = "data/") -> None:
+    generate_directory(preprocessed_data_dir)
     _counter = 0
-
-    if len(os.listdir(data_dir)) == 0:
+    if len(os.listdir(preprocessed_data_dir)) == 0:
         for genre in gtzan_genres:
             path = os.path.join(genre_dir, genre)
             audio_files = os.listdir(path)
             for audio_file in audio_files:
                 chopped_audio = chop_audio(audio_file=os.path.join(path, audio_file))
-                _counter = export_to_dir(genre, data_dir, chopped_audio, _counter)
+                _counter = export_to_dir(
+                    genre, preprocessed_data_dir, chopped_audio, _counter
+                )
                 print(audio_file)
     else:
         print("data already preprocessed")
@@ -63,4 +47,3 @@ def preprocess_data() -> None:
 
 if __name__ == "__main__":
     preprocess_data()
-    count_items_in_dataset()
