7d763b522bb71e4a9b8a01d84159d5d816351c59
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-03-27 23:45:11 +0200

    Updated pipeline

--- projects/music_genre_classification/music_genre_classification/flyte_pipeline.py
+++ projects/music_genre_classification/music_genre_classification/flyte_pipeline.py
@@ -6,6 +6,7 @@ from train import train, evaluate
 from dataclasses import dataclass
 from dataclasses_json import dataclass_json
 import torch
+import bentoml
 from typing import List, Dict
 from torch.utils.data import DataLoader, Dataset
 from flytekit import task, workflow
@@ -78,12 +79,43 @@ def evaluate_model(model: MusicGenreClassificationModel, test_dataloader: DataLo
     return evaluate(model=model, test_dataloader=test_dataloader)
 
 
+@task
+def save_model(model: MusicGenreClassificationModel, artifact_name: str) -> None:
+    bentoml.pytorch.save(name=artifact_name, model=model)
+
+
+# @task
+# def test_model_deployment(artifact_name: str, target_names: any) -> None:
+#     test_input = [5.9, 3.0, 5.1, 1.8]
+#     test_output = "virginica"
+#
+#     test_runner = bentoml.pytorch.load_runner(tag=artifact_name)
+#     x = Variable(torch.FloatTensor(test_input))
+#     prediction = test_runner.run(x)
+#     print(target_names[np.where(prediction == 1.0)[0]])
+#
+#     assert test_output == target_names[np.where(prediction == 1.0)[0]]
+
+
+@task
+def build_bentoml_service() -> None:
+    bentoml.build(
+        "bento_service.py:svc",
+        include=["*.py"],
+        description="file:../../../README.md",
+        python=dict(
+            packages=["pytorch"]
+        )
+    )
+
+
 @workflow
 # def music_genre_classifier() -> (Dict[str, any], MultiTemporalFeatureMap):
 def music_genre_classifier() -> MusicGenreClassificationModel:
     training_params = TrainingParameters()
     download_dataset(target_folder=".", download=True)
     data_preprocessing()
+    # train_dataloader, validation_dataloader, test_dataloader = split_dataset()
     train_dataloader, validation_dataloader, test_dataloader = split_dataset(
         train_split_pc=0.01, validation_split_pc=0.01, test_split_pc=0.98
     )
@@ -97,8 +129,11 @@ def music_genre_classifier() -> MusicGenreClassificationModel:
         gradient_clip=training_params.gradient_clip
     )
 
-    evaluation_data = evaluate_model(model=model, test_dataloader=validation_dataloader)
     # evaluation_data = evaluate_model(model=model, test_dataloader=test_dataloader)
+    evaluation_data = evaluate_model(model=model, test_dataloader=validation_dataloader)
+
+    save_model(model=model, artifact_name="genre_classification_model")
+    build_bentoml_service()
 
     # for sending to model tracker
     model_artifacts: dict = {
