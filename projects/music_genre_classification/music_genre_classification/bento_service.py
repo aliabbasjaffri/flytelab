ef3c0d84ac968c0122bffcd3605184d04346e3d6
Ali Abbas Jaffri <ali.abbasjaffri@live.com>
2022-03-31 22:42:07 +0200

    Updated deployment logic

--- projects/music_genre_classification/music_genre_classification/bento_service.py
+++ projects/music_genre_classification/music_genre_classification/bento_service.py
@@ -15,7 +15,19 @@ svc = bentoml.Service(name=artifact_name, runners=[genre_classifier_runner])
 
 @svc.api(input=File(), output=JSON())
 def classify(input_music_file) -> str:
-    x = torchaudio.load(input_music_file)[0]
+    audio_seg = AudioSegment.from_wav(input_music_file)
+    audio_length = len(audio_seg)
+
+    print(audio_length)
+
+    if audio_length > segment_size:
+        preprocessed_audio = chop_audio(audio_file=input_music_file, segment_size=segment_size, max_size=max_size)[0]
+        preprocessed_audio = preprocessed_audio.export(input_music_file, format="wav")
+    else:
+        print("came here...")
+        preprocessed_audio = input_music_file
+
+    x = torchaudio.load(preprocessed_audio)[0]
     prediction_probabilities = genre_classifier_runner.run(x)
     print(prediction_probabilities.max())
     value = gtzan_genres[(prediction_probabilities.max()).nonzero(as_tuple=True)[0]]
